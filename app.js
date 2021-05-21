af30e5e7f77e84d114cafa7fd987c48fe28e931c
Inoxia25 <66139520+Inoxia25@users.noreply.github.com>
2021-05-21 14:12:55 +0530

    made code cleaner

--- app.js
+++ app.js
@@ -391,19 +391,6 @@ app.listen(port, function() {
   }
   
 });
-app.get('/refresh_token', function(req, res) {
-
-  // requesting access token from refresh token
-  var refresh_token = req.query.refresh_token;
-  var authOptions = {
-    url: 'https://accounts.spotify.com/api/token',
-    headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
-    form: {
-      grant_type: 'refresh_token',
-      refresh_token: refresh_token
-    },
-    json: true
-  };
 
 
  
